ac33cf1696b564bf791f63f4be7e634fd78c7198
MauroBorzi <mauroborzi91@gmail.com>
2025-07-04 17:00:39 +0200

    [upd]mail

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
 console.log('-----mail-----')
 
-// dichiarazioni variabili
+// dichiarazione variabili
 const mailList = [
   "tonio.cartonio@mail.it", 
   "ciccio.pasticcio@mail.it", 
   "pippo.baudo@mail.it", 
   "aieie.brazzof@mail.it"
 ]
-const userMail = parseInt(prompt(`Inserisci la tua email`))
+const userMail = prompt(`Inserisci la tua email`)
+let found = false
 
 // ciclo "for" per controllare se la mail inserita è presente nel array
 
-for(let i=0; i<mailList.length; i++){
+for(let i = 0; i < mailList.length; i++){
   if(mailList[i] === userMail){
-console.log(`Email valida. L'utente potrà prendere parte all'evento.`)
-  }
-  else{
-    console.log(`Email non valida. L'utente non potrà prendere parte all'evento.`)
+    found = true
   }
 }
 
+if(found){
+  console.log(`Email valida. L'utente potrà prendere parte all'evento.`)
+}
+else{
+  console.log(`Email non valida. L'utente non potrà prendere parte all'evento.`)
+}
 
 console.log('-----dadi-----')
 
-// dichiarazioni variabili
+// dichiarazione variabili
  const randomPcNumber = Math.floor(Math.random() * 6) +1
  const randomUserNumber = Math.floor(Math.random() * 6) +1
 
-//  regole per determinare il vincitore del lancio
+// regole per determinare il vincitore del lancio
 if(randomPcNumber>randomUserNumber){
   console.log(`Pc fa ${randomPcNumber}. Utente fa ${randomUserNumber}. Vince Pc.`)
 } 
